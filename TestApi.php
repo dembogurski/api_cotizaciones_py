857e49b65e29f08c95d9207a0b0c8a99a38e7671
dembogurski <dembogurski@gmail.com>
2023-01-10 13:04:14 -0300

    Ejemplo en PHP mejorado

--- TestApi.php
+++ TestApi.php
@@ -1,6 +1,12 @@
 <?php
 
-  $json =  file_get_contents('http://testing.marijoa/marijoa/utils/SET/HistoricoCotizacionesSETParaguay.class.php?anio=2022&mes=08&dia=14');
+  //$json =  file_get_contents('http://testing.marijoa/marijoa/utils/SET/HistoricoCotizacionesSETParaguay.class.php?anio=2022&mes=08&dia=14');
+
+  $json =  file_get_contents('http://testing.marijoa/marijoa/utils/SET/HistoricoCotizacionesSETParaguay.class.php?anio=2022&mes=08');
+
+  $data = json_decode($json,true);
+    
+  echo $data["01"]['dolar']['compra'];
 
 ?>
 
