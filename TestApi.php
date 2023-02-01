a3d022d04bea9c901b2843dcff997f59e9654d12
Doglas A. Dembogurski Feix <dembogurski@gmail.com>
2023-02-01 07:55:28 -0300

    Update TestApi.php
    Correccion de URL

--- TestApi.php
+++ TestApi.php
@@ -1,8 +1,8 @@
 <?php
 
-  //$json =  file_get_contents('http://testing.marijoa/marijoa/utils/SET/HistoricoCotizacionesSETParaguay.class.php?anio=2022&mes=08&dia=14');
+  //$json =  file_get_contents('http://<IP_TU_SERVIDOR>/SET/HistoricoCotizacionesSETParaguay.class.php?anio=2022&mes=08&dia=14');
 
-  $json =  file_get_contents('http://testing.marijoa/marijoa/utils/SET/HistoricoCotizacionesSETParaguay.class.php?anio=2022&mes=08');
+  $json =  file_get_contents('http://<IP_TU_SERVIDOR>/SET/HistoricoCotizacionesSETParaguay.class.php?anio=2022&mes=08');
 
   $data = json_decode($json,true);
     
